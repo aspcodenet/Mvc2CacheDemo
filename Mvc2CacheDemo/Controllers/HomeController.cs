48975715b853f94cb675c385e7aefa6119ef1a2f
Stefan Holmberg <stefan.holmberg@systementor.se>
2020-05-11 11:26:07 +0200

    Add project files.

---
+++ Mvc2CacheDemo/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mvc2CacheDemo.Models;
+using Mvc2CacheDemo.ViewModels;
+
+namespace Mvc2CacheDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [ResponseCache(Duration = 120, Location = ResponseCacheLocation.Any)]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+       [ResponseCache(Duration = 120, Location = ResponseCacheLocation.Any, VaryByQueryKeys = new[] { "num" })]
+       public IActionResult Product(int num)
+        {
+            var model = new ProductViewModel();
+            model.Id = num;
+            if (model.Id == 1)
+            {
+                model.Name = "Banan";
+                model.Price = 12;
+            }
+            else if (model.Id == 2)
+            {
+                model.Name = "Läsk";
+                model.Price = 4;
+            }
+
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mvc2CacheDemo/Services/IMyService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Mvc2CacheDemo.Services
+{
+    public interface IMyService
+    {
+        string GetName();
+        int GetAnother(int b);
+    }
+
+    class MyService : IMyService
+    {
+        public string GetName()
+        {
+            return "Stefan";
+        }
+
+        public int GetAnother(int b)
+        {
+            //Fake fetching from slow service...
+            System.Threading.Thread.Sleep(1000);
+
+            return b + 10;
+        }
+    }
+    class CachedMyService : IMyService
+    {
+        private readonly IMyService _inner;
+        private readonly IMemoryCache _cache;
+
+        public CachedMyService(IMyService inner, IMemoryCache cache)
+        {
+            _inner = inner;
+            _cache = cache;
+        }
+        public string GetName()
+        {
+            return _inner.GetName();
+        }
+
+        public int GetAnother(int b)
+        {
+            return _cache.GetOrCreate($"MyService-{b}", e => _inner.GetAnother(b));
+        }
+    }
+}
